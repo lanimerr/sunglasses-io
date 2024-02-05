578539b3d69aab0215d81853cd48fc6d10eee506
lanimerr <milani.m7696@gmail.com>
2024-02-05 13:12:02 -0500

    2nd submit eval

--- app/server.js
+++ app/server.js
@@ -1,31 +1,208 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml'); // Replace './swagger.yaml' with the path to your Swagger file
-const app = express();
+const uid = require('rand-token').uid;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const Router = require('router');
+const queryString = require('querystring');
+const { url } = require('url');
+const router = Router();
+const finalHandler = require('finalhandler');
 
-app.use(bodyParser.json());
+let accessToken = [];
+chai.use(chaiHttp);
 
-// Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
+//state variables
+const PORT = 3001;
+let brands = [];
+let users = [];
+let products = [];
 
-// Error handling
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+// Setup router
+
+router.use(bodyParser.json());
+
+const server = http.createServer(function (req, res) {
+  	router(req, res, finalHandler(req, res))
+}).listen(PORT, () => {
+  console.log("Node is running on 3001")
 });
 
-// Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Starting the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+//Load data
+fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+  if (error) throw error;
+  brands = JSON.parse(data);
+  console.log(`Server setup: ${brands.length} brands loaded`)
 });
 
-module.exports = app;
+fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+  if (error) throw error;
+  products = JSON.parse(data);
+  console.log(`Server setup: ${products.length} products loaded`)
+});
+
+fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+  if (error) throw error;
+  users = JSON.parse(data);
+  console.log(`Server setup: ${users.length} users loaded`)
+});
+
+//Setting up routers
+router.get('/brands', function(req, res) {
+  if(!brands) {
+    res.writeHead(404, "Nothing to return");
+  }
+  res.writeHead(200, {"Content-Type": "application/json"});
+  return res.end(JSON.stringify(brands));
+});
+
+
+router.get('/products', function(req, res) {
+  if(!products) {
+    res.writeHead(404, "Nothing to return");
+  };
+res.writeHead(200, {"Content-Type": "application/json"});
+return res.end(JSON.stringify(products));
+});
+
+
+router.get('/brands/:brandId/products', function(req, res) {
+  let {brandId} = req.params;
+  if (brandId > 5) {
+    res.writeHead(400, "No parameter with that ID")
+    return res.end("No brand with that id please choose 1 through 5")
+  }
+
+  let match = [];
+  let getProduct = function (brand, product, parameter) {
+    let myParam = parameter.toString();
+    product.forEach(prod => {
+     let myBrand = brand.find(e =>
+       e.id === prod.categoryId);
+       if (myBrand.id === myParam) {
+         match.push(myBrand.name)
+         match.push(prod.name)
+         match.push(prod.price)
+         match.push(prod.description)
+         match.push(prod.imageUrls)
+       }
+   })
+  }
+
+   getProduct(brands, products, brandId);
+  products = match;
+  console.log(products)
+  res.writeHead(200, {"Content-Type": "application/json"});
+  return res.end(JSON.stringify(products));
+
+
+})
+
+router.post('/login', function(req, res)  {
+  if (req.body.username && req.body.password) {
+    let user = users.find((user) => {
+      return user.login.username == req.body.username && user.login.password == req.body.password;
+    });
+
+    if (user) {
+      let newUserAuth = {
+        username: user.login.username,
+        lastUpdated: new Date(),
+        token: uid(16)
+      }
+      accessToken.push(newUserAuth);
+      console.log(JSON.stringify(accessToken));
+      console.log(newUserAuth)
+      console.log(accessToken[0])
+      let match = users.find(user => user.username === accessToken.username)
+      console.log(match.cart);
+      res.writeHead(200, {"Content-Type": "application/json"});
+      return res.end(JSON.stringify(accessToken));
+    } else {
+      res.writeHead(401, "Invalid Login");
+      return res.end();
+    }
+  } else {
+    res.writehead(400, "Incorrect format");
+    return res.end();
+  }
+})
+
+router.get("/me/cart", function (req, res) {
+  let authUser = users.find(user => user.username === accessToken.username)
+
+  if (authUser) {
+    res.writeHead(200, "Access granted")
+    return res(JSON.stringify(authUser.cart))
+  } else {
+    res.writeHead(401, "Unauthorized to access shopping cart");
+    return res.end();
+  }
+})
+
+router.post('/me/cart', function (req, res) {
+  let authUser = users.find(user => user.username === accessToken.username)
+
+  let product = products.find((p) => {
+    return p.id && p.categoryId && p.price;
+  })
+
+  console.log(users)
+
+  if (authUser) {
+    let cart = authUser.cart;
+    cart.push(product)
+    res.writeHead(201, "Access granted")
+    return res(JSON.stringify(cart))
+  } else {
+    res.writeHead(401, "Unauthorized to access shopping cart");
+    return res.end();
+  }
+});
+
+router.delete('/me/cart/:productid', function (req, res) {
+  let {productid} = req.params;
+  let stringProduct = productid.toString();
+  
+  let authUser = users.find(user => user.username === accessToken.username)
+
+  let cart = authUser.cart;
+  let index = cart.indexOf(stringProduct);
+
+  if (authUser) {
+    let newCart = cart.splice(index);
+    res.writeHead(200, "Access granted")
+    return res(JSON.stringify(newCart))
+    } else {
+      res.writeHead(401, "Unauthorized to access shopping cart");
+      return res.end();
+    }
+})
+
+router.post('/me/cart/:productid', function (req, res) {
+  let {productid} = req.params;
+  let stringProduct = productid.toString();
+
+  let productToEdit = products.filter(p => {
+    return p.id === stringProduct;
+  })
+  
+  let authUser = users.find(user => user.username === accessToken.username)
+
+  let cart = authUser.cart;
+
+
+  if (authUser) {
+    let addCart = cart.push(productToEdit);
+    res.writeHead(200, "Access granted")
+    return res(JSON.stringify(addCart))
+    } else {
+      res.writeHead(401, "Unauthorized to access shopping cart");
+      return res.end();
+    }
+})
+
+module.exports = server;
+
+   
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,219 @@
+const http = require('http');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
+const server = require('../app/server');
+const expect = chai.expect;
 
-const should = chai.should();
+const removeAll = function () {
+  products = [];
+}
+
 chai.use(chaiHttp);
+chai.should();
 
-// TODO: Write tests for the server
 
-describe('Brands', () => {});
+describe('Server API', () => {
+  describe('GET brands', () => {
+    it('should get all the brands', (done) => {
+      chai
+        .request(server)
+        .get('/brands')
+        .end((err, res) => {
+          res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.be.eql(5);
+            done();
+        });
+    });
+  });
 
-describe('Login', () => {});
+    describe('/GET sunglasses by brand', () => {
+      it('should get all sunglasses of a specific brand', (done) => {
+        chai.request(server)
+          .get(`/brands/:brandId/products`) 
+          .end((err, res) => {
+            // console.log(res.status); 
+            // console.log(res.body);
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.be.eql(0);
+            done();
+          });
+      });
+    });
+  
+    describe('GET sunglasses by brand', () => {
+      it('should return an error if a brand with no sunglasses is entered', (done) => {  
+        chai.request(server)
+          .get(`/brands/3/products`) 
+          .end((err, res) => {
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+  });
 
-describe('Cart', () => {});
+
+  describe('Products', () => {
+    beforeEach(() => {
+      removeAll();
+    })
+
+    //this one has an error 
+  
+    describe('/GET Products', () => {
+      it("it should GET all of the products", done => {
+        chai
+        .request(server)
+        .get('/products')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+      });
+    });
+    
+    describe('/GET Products/:id', () => {  
+      it("it should GET all of the products that match brandId", done => {
+        chai
+        .request(server)
+        .get('/brands/:brandId/products')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+      });
+    });
+  });
+  
+   
+describe('Login', () => {
+  describe('/POST login data', () => {
+    it("post all required login data", done => {
+      let user = {
+        "username": "greenlion235",
+        "password": "waters"
+      }
+      chai
+      .request(server)
+      .post('/login')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(res.body[0].username).to.equal('greenlion235');
+        expect(res.body[0].lastUpdated).to.be.a('string');
+        expect(res.body[0].token).to.have.lengthOf(16);
+        const token = res.body[0].token;
+        done();
+      });
+    });
+  });
+});
+
+  describe('/GET Cart', () => {
+    it("it should GET items in users cart", done => {
+      let user = {
+        "username": "greenlion235",
+        "password": "waters"
+      }
+      let agent = chai.request.agent(server)
+      agent
+      .post('/login')
+      .send(user)
+      done()
+
+      .then(function (res) {
+        const token = res.body[0].token;
+        expect(res).to.have.cookie(token)
+        return agent.get('/me/cart')
+        .then(function (res) {
+           expect(res).to.have.status(200);
+           agent.close();
+        });
+    });
+  });
+});
+
+  describe('/POST Cart', () => {
+    it("it should POST an item in users cart", done => {
+      let user = {
+        "username": "greenlion235",
+        "password": "waters"
+      }
+      let agent = chai.request.agent(server)
+      agent
+      .post('/login')
+      .send(user)
+      done()
+
+      .then(function (res) {
+        const token = res.body[0].token;
+        expect(res).to.have.cookie(token)
+        return agent.post('/me/cart')
+        .then(function (res) {
+           expect(res).to.have.status(201);
+           expect(res).to.be.an('array');
+           agent.close();
+        });
+    });
+  });
+});
+
+  describe('/DELETE Cart/productId', () => {
+    it("it should DELETE an item in users cart", done => {
+      let user = {
+        "username": "greenlion235",
+        "password": "waters"
+      }
+      let agent = chai.request.agent(server)
+      agent
+      .post('/login')
+      .send(user)
+      done()
+
+      .then(function (res) {
+        const token = res.body[0].token;
+        expect(res).to.have.cookie(token)
+        return agent.delete('/me/cart/:productid')
+        .then(function (res) {
+           expect(res).to.have.status(201);
+           expect(res).to.be.an('array');
+           agent.close();
+        });
+    });
+  });
+});
+
+  describe('/POST Cart/productId', () => {
+    it("it should Update the quantity of an item in users cart", done => {
+      let user = {
+        "username": "greenlion235",
+        "password": "waters"
+      }
+      let agent = chai.request.agent(server)
+      agent
+      .post('/login')
+      .send(user)
+      done()
+
+      .then(function (res) {
+        const token = res.body[0].token;
+        expect(res).to.have.cookie(token)
+        return agent.post('/me/cart/:productid')
+        .then(function (res) {
+           expect(res).to.have.status(201);
+           expect(res).to.be.an('array');
+           agent.close();
+           done();
+        });
+    });
+  });
+});
+
+
+//mocha test/server.test.js
